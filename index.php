20f137e0cc43272a05699b2752534f82b6662bcd
Alex Weininger <alex.weininger@live.com>
2015-08-03 19:42:08 -0700

    debug

--- index.php
+++ index.php
@@ -5,16 +5,17 @@ require_once 'config.php';
 try {
     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+echo '1'
     $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
     $oResult = $oStmt->fetchAll();
-
+echo '2'
     foreach ($oResult as $aRow) {
         print_r($aRow['data']);
     }
-
+echo '3'
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
+    echo '4'
 }
 
 echo 'Hello World!'
