955ae4bab9e48d96a552591c3d33e80568c3a1f4
Alex Weininger <alex.weininger@live.com>
2015-08-03 19:43:20 -0700

    fix

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ require_once 'config.php';
 try {
     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo '1'
+echo '1';
     $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
     $oResult = $oStmt->fetchAll();
-echo '2'
+echo '2';
     foreach ($oResult as $aRow) {
         print_r($aRow['data']);
     }
-echo '3'
+echo '3';
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
-    echo '4'
+    echo '4';
 }
 
-echo 'Hello World!'
+echo 'Hello World!';
 
 ?>
