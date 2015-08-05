8014403d7007a5f2b2ce07b7a98253a501d30e8f
Alex Weininger <alex.weininger@live.com>
2015-08-05 16:52:54 -0700

    fixed admin.php

--- home/admin.php
+++ home/admin.php
@@ -36,7 +36,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class=""><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
-        <li class="active"><a href="projects.php">Projects</a></li>
+        <li class=""><a href="projects.php">Projects</a></li>
        <li><a href="about.php">About</a></li>
       </ul>
      
