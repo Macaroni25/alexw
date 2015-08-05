846789f47bf52ec9339c225d968a259198dc7f6a
Alex Weininger <alex.weininger@live.com>
2015-08-05 14:39:56 -0700

    fix # 13
    lucky #13

--- db.php
+++ db.php
@@ -22,10 +22,22 @@ $db_connection = new PDO( "mysql:host=" . $hostname . ";dbname=" . $database, $u
 $results = $db_connection->query( 'SELECT name  FROM projects' );
 
 foreach ( $results as $row ) {
-	echo '<tr><th>' . $row['name'] . '</th>';
-        echo '<th>' . $row['description'] . '</th>';
-        echo '<th>' . $row['status'] . '</th>';
-        echo '<th>' . $row['link'] . '</th></tr>';
+	echo '<tr> <th>' . $row['name'] . '</th>';
+}
+$results = $db_connection->query( 'SELECT description  FROM projects' );
+
+foreach ( $results as $row ) {
+	echo '<th>' . $row['description'] . '</th>';
+}
+$results = $db_connection->query( 'SELECT status  FROM projects' );
+
+foreach ( $results as $row ) {
+	echo '<tr> <th>' . $row['status'] . '</th>';
+}
+$results = $db_connection->query( 'SELECT link  FROM projects' );
+
+foreach ( $results as $row ) {
+	echo '<tr> <th>' . $row['link'] . '</th></tr>';
 }
 
 
