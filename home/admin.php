436011f47a460e0bae054a012311bd57022c382d
Alex Weininger <alex.weininger@live.com>
2015-08-05 16:57:11 -0700

    admin.php

--- home/admin.php
+++ home/admin.php
@@ -83,5 +83,49 @@ status <input name="status" type="text"><br>
 link <input name="link" type="text"><br>
 <input type="Submit" value="Submit" name="Submit"><br>
 </font>
+<div class="container">
+     
+
+<table class="table table-bordered">
+
+<?php
+$con=mysqli_connect("us-cdbr-azure-northcentral-a.cleardb.com","b3262821ada9ac","cacb753a","alexwdb");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM projects");
+
+echo '
+<tr>
+<th>Project Name</th>
+<th>Description</th>
+<th>Status</th>
+<th>Link</th>
+</tr>';
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['name'] . "</td>";
+echo "<td>" . $row['description'] . "</td>";
+echo "<td>" . $row['status'] . "</td>";
+echo "<td>" . '<a href="' . $row['link'] .'">Go to Project</a>' . "</td>";
+echo "</tr>";
+}
+
+mysqli_close($con);
+?>
+  </table>
+ </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
 </body>
 </html>
