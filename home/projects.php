20a932155efc6007ecb84ab2c338b6e3521bcaf5
Alex Weininger <alex.weininger@live.com>
2015-08-05 15:30:54 -0700

    fix #6

--- home/projects.php
+++ home/projects.php
@@ -95,4 +95,9 @@ mysqli_close($con);
 ?>
   </table>
  </div>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
+</html>
