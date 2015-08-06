461deebad5076bcabc72e91ce568dd787740a27c
Alex Weininger <alex.weininger@live.com>
2015-08-05 17:27:43 -0700

    fix???

--- home/deleteproject.php
+++ home/deleteproject.php
@@ -1,7 +1,15 @@
 <?php
+$con=mysqli_connect("us-cdbr-azure-northcentral-a.cleardb.com","b3262821ada9ac","cacb753a","alexwdb");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 if(isset($_POST['deleteItem']) and is_numeric($_POST['deleteItem']))
 {
   // here comes your delete query: use $_POST['deleteItem'] as your id
   $sql = "DELETE FROM projects WHERE id=$_POST['deleteItem']";
 }
+mysqli_close($con);
+?>
