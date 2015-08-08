ac568ddedfcda00bcda7ed0ea8f61ef941513801
Alex Weininger <alex.weininger@live.com>
2015-08-08 10:10:01 -0700

    added comments

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+// login info for mySQL database on azure
+$user = 'b3262821ada9ac';
+$pass = 'cacb753a';
+$hostname = 'us-cdbr-azure-northcentral-a.cleardb.com';
+$database = 'alexwdb';
+?>
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-  header( 'Location: home/index.php' ) ;
-  
-  
+  header( 'Location: home/index.php' ) ; //redirect to /home/index.php
 ?>
