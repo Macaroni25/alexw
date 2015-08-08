de95210f7aed1118855917055edee26184bcdf47
Alex Weininger <alex.weininger@live.com>
2015-08-08 10:00:04 -0700

    commit #3

--- thebutton/click.php
+++ thebutton/click.php
@@ -1,12 +1,1 @@
-   
-<?php 
- if(isset($_POST['submit'])) { 
- foo(); 
- } 
- ?> 
- 
- <form action="<?=$_SERVER['PHP_SELF'];?>" method="post"> 
- <input type="submit" name="submit" value="Click Me"> 
- </form> 
-
 
