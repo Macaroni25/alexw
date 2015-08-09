014696ef0ad2f57d994ada3aea2f4652779a68d4
Alex Weininger <alex.weininger@live.com>
2015-08-09 13:51:03 -0700

    geogram

--- css-test/js/main.js
+++ css-test/js/main.js
@@ -1,15 +1,14 @@
 
 $(document).ready(function () {
 
-	$(".nodebutton").click(function () {
+	
 
-		var clickedid = $(this).attr("id");
 
-		$(".text").html("<p>You clicked button # " + clickedid + "</p>");
+
 		
 
 		});
-	});
+
 
 
 
