c72154a4d28c7cfde80abf454f833cde01de3701
Alex Weininger <alex.weininger@live.com>
2015-08-09 14:23:24 -0700

    table styleing

--- home/projects.php
+++ home/projects.php
@@ -20,43 +20,11 @@
  
  <body>
 
- <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="">Alex Weininger</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class=""><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
-        <li class="active"><a href="projects.php">Projects</a></li>
-       <li><a href="about.php">About</a></li>
-      </ul>
-     
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="admin.php">Admin Portal</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+ <nav class="navbar">
+				<a id="" class="nodebutton" href="index.html">Home</a>
+				<a id="" class="nodebutton" href="projects.php">Projects</a>
+				<a id="" class="nodebutton" href="#">About</a>
+			</nav>
 
 <div class="container">
      
