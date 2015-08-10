2cf4b51c8ccf51f5229b6bcafe93b75ae4da3fca
Alex Weininger <alex.weininger@live.com>
2015-08-09 17:20:40 -0700

    removed nimble

--- nimble.css/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$url = "https://raw.githubusercontent.com/Macaroni25/nimble.css/master/index.html"; //write your raw.github.com URL
-$gethtml = file_get_contents($url);
-echo $gethtml; //output the content in the page
-?>
