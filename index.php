ec9c3edb4b80bc723e7883c1e50a8aaeb1c49bdb
Alex Weininger <alex.weininger@live.com>
2015-08-09 18:11:26 -0700

    meta

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+
+    <meta name="description" content="a website made by Alex Weininger" />
+    
+    <title>Alex Weininger</title>
+    
+    </head>
+    <body>
+      </body>
 <?php
   header( 'Location: home/index.php' ) ; //redirect to /home/index.php
 ?>
+</html>
