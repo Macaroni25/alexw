cdac19ba07bae3bb77084100b376ad4be14010e5
Alex Weininger <alex.weininger@live.com>
2015-08-11 18:25:15 -0700

    removed

--- bootstrap-home/index.php
+++ bootstrap-home/index.php
@@ -52,19 +52,7 @@ include 'projects.php';
 ?>
 </div>
 </div>
-<div class="panel panel-default">
-<div class="panel-heading">
-<h3 class="panel-title">Geogram</h3>
-</div>
 
-
-
-<div class="panel-body">
-<?php
-include '../geogram/geogram.php';
-?>
-</div>
-</div>
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
